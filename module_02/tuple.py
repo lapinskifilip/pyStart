c72f3ccb393ca4820fbc76d3315c260658887b52
Filip <lapinski.filipp@gmail.com>
2021-03-28 14:03:57 +0200

    some changes

--- module_02/tuple.py
+++ module_02/tuple.py
@@ -4,7 +4,7 @@ print(f'Liczb podzielny przez 6 z zakresu od 12 do 1024 jest: {len(numbers)}')
 print(f'Trzy pierwsze liczby z tego zakresu to: {numbers[:3]}')
 print(f'Przedostatni element tego zakresu to: {numbers[-2]}')
 print(f'Co szosty element zaczynajac od 4: {numbers[4::6]}')
-print(f'Co trzeci element licząc od końca: {numbers[-1:1:-3]}')
+print(f'Co trzeci element licząc od końca: {numbers[::-3]}')
 
-print(f'Srednia ostatnich 10 cyfr to: {int(sum(numbers[-10::]) / 10)}')
+print(f'Srednia ostatnich 10 cyfr to: {int(sum(numbers[-10:]) / 10)}')
 

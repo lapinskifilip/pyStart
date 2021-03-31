2d6e1236b329eff29d62b092020677abb5000874
Filip <lapinski.filipp@gmail.com>
2021-03-31 13:51:53 +0200

    new exercises

--- module_02/exercises/lesson10ex3.py
+++ module_02/exercises/lesson10ex3.py
@@ -1,8 +1,13 @@
-from math import sqrt
+number = int(input('Please give me a number: '))
 
-number = int(input('Podaj prosze liczbe: '))
-
-if number < 2:
-    print('Nie jest to liczba pierwsza')
-elif number % number == 0:
-    print('Jest to liczba pierwsza')
+if number <= 1:
+    print(f'{number} is not a prime number')
+else:
+    if number == 2:
+        print(f'{number} is a prime number')
+        exit()
+    for i in range(2, (number // 2) + 1):
+        if number % i == 0:
+            print(f'{number} is not a prime number')
+            exit()
+    print(f'{number} is a prime number')

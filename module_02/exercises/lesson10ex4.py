8fd3faa4e5059b8acc0e7ef8b0b6a404b4f26db3
Filip <lapinski.filipp@gmail.com>
2021-03-31 13:56:07 +0200

    some changes

--- module_02/exercises/lesson10ex4.py
+++ module_02/exercises/lesson10ex4.py
@@ -1,6 +1,19 @@
-
-
+for i in range(0, 6, 1):
+    for j in range(1, i + 1):
+        print(j, end=' ')
+    print("\r")
 for i in range(4, 0, -1):
     for j in range(1, i + 1):
         print(j, end=' ')
     print("\r")
+
+#output
+# 1
+# 1 2
+# 1 2 3
+# 1 2 3 4
+# 1 2 3 4 5
+# 1 2 3 4
+# 1 2 3
+# 1 2
+# 1
